54a642bb9ec3e983c8093cdace726d8471f4377d
Boda Song <boda2@illinois.edu>
2021-10-13 16:08:29 -0500

    MP6 submission

--- mp6/app.py
+++ mp6/app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import os
+import flask
 app = Flask(__name__)
 
 # Route for "/" for a web-based interface to this micro-service:
@@ -11,5 +13,30 @@ def index():
 @app.route('/extract', methods=["POST"])
 def extract_hidden_gif():
   from flask import send_file, request
-
   # ...your code here...
+
+  #create new directory
+  directory = "temp"
+  #parent_dir = "/Users/bodasong/Documents/cs240/cs240-fa21-boda2/mp6"
+  #path = os.path.join(parent_dir, directory)
+  os.makedirs(directory, exist_ok=True)
+
+  #copy file
+  if request.method == 'POST':
+    if 'png' not in request.files:
+      return "No such file", 500
+    file = request.files['png']
+    name = file.filename
+    if name == '':
+      return "Invalid file name", 500
+    if file:
+      file.save(os.path.join("temp", name))
+    #run the program
+    cmd = "./png-extractGIF temp/" + name + " temp/output.gif"
+    return_value = os.system(cmd)
+    print(return_value)
+    if return_value != 0:
+      return "png extract unsuccessful", 500
+    return send_file("temp/output.gif")
+    
+  
